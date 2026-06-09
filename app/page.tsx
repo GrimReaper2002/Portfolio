1cab7e41e08f545df7fe3f29d08acbb9aa854eb7
Ram Kishore Namala <92012986+GrimReaper2002@users.noreply.github.com>
2026-06-08 23:48:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import { Suspense } from 'react'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
